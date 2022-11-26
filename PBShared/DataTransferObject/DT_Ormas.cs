90b4ad5577f7b959c1369b86ffd10ef1501b6347
nanda1211 <nanda.destyandana@visionet.co.id>
2022-11-26 07:11:55 +0700

    Data Ormas Web API CRUD - OK

---
+++ PBShared/DataTransferObject/DT_Ormas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBShared.DataTransferObject
+{
+    public class DT_Ormas
+    {
+         public string kodeOrmas { get; set; }
+         public string namaOrmas { get; set; }
+         public string alamat { get; set; }
+         public string namaPimpinan { get; set; }
+         public string nomorTerdaftar { get; set; }
+         public string bidangKegiatan { get; set; }
+         public string keterangan { get; set; }
+         public string? suku { get; set; }
+         public bool berbadanHukum { get; set; }
+         public byte[] logoOrmas { get; set; }
+         public string nomorTelp { get; set; }
+         public byte[]? photoPimpinan { get; set; }
+         public bool aktif { get; set; }
+         public string inputUN { get; set; }
+         public DateTime inputTime { get; set; }
+         public string? modifiedUN { get; set; }
+         public DateTime? modifiedTime { get; set; }
+    }
+}
---
+++ PBWebAPI/Controllers/DataOrmasController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PBShared.DataTransferObject;
+using PBShared.DBContexts;
+using PBShared.Models;
+
+namespace PBWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataOrmasController : ControllerBase
+    {
+        private readonly ILogger<DataOrmasController> _logger;
+        private PBManageServiceContext _dbContext;
+
+        public DataOrmasController(ILogger<DataOrmasController> logger,
+            PBManageServiceContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+       
+        [HttpPost]
+        [Route("CreateDataOrmas")]
+        public GeneralReturnValue CreateDataOrmas(DT_Ormas input)
+        {
+            GeneralReturnValue res = new GeneralReturnValue();
+
+            try
+            {
+
+                input.inputTime = DateTime.Now;
+                input.modifiedUN = null;
+                input.modifiedTime = null;
+
+                _dbContext.DT_Ormas.Add(input);
+                _dbContext.SaveChanges();
+
+                var dtOrmas = (from a in _dbContext.DT_Ormas
+                                 select a).OrderByDescending(n => n.inputTime).FirstOrDefault();
+                
+                List<string> data = new List<string>();
+                
+                if (dtOrmas != null)
+                {
+                    data.Add(dtOrmas.kodeOrmas);
+                    data.Add(dtOrmas.namaOrmas);
+                }
+
+                res.status = "success";
+                res.message = "Data Ormas berhasil disimpan";
+                res.data = data;
+            }
+            catch (Exception ex)
+            {
+                res.status = "failed";
+                res.message = ex.Message;
+                res.data = null;
+            }
+
+            return res;
+        }
+
+        [HttpGet]
+        [Route("GetDataOrmas/{kodeOrmas}")]
+        public DT_Ormas GetDataOrmas(string kodeOrmas)
+        {
+            DT_Ormas res = new DT_Ormas();
+
+            try
+            {
+                var data = (from a in _dbContext.DT_Ormas
+                            where a.kodeOrmas == kodeOrmas
+                            select a).FirstOrDefault();
+
+                if(data != null)
+                {
+                    res = data;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return res;
+        }
+
+        [HttpPut]
+        [Route("UpdateDataOrmas")]
+        public GeneralReturnValue UpdateDataOrmas(DT_Ormas input)
+        {
+            GeneralReturnValue res = new GeneralReturnValue();
+
+            try
+            {
+                input.modifiedTime = DateTime.Now;
+
+                _dbContext.DT_Ormas.Update(input);
+                _dbContext.SaveChanges();
+
+                var dtOrmas = (from a in _dbContext.DT_Ormas
+                               select a).OrderByDescending(n => n.modifiedTime).FirstOrDefault();
+
+                List<string> data = new List<string>();
+
+                if (dtOrmas != null)
+                {
+                    data.Add(dtOrmas.kodeOrmas);
+                    data.Add(dtOrmas.namaOrmas);
+                }
+
+                res.status = "success";
+                res.message = "Data Ormas berhasil diupdate";
+                res.data = data;
+            }
+            catch (Exception ex)
+            {
+                res.status = "failed";
+                res.message = ex.Message;
+                res.data = null;
+            }
+
+            return res;
+        }
+
+        [HttpDelete]
+        [Route("DeleteDataOrmas/{kodeOrmas}")]
+        public GeneralReturnValue DeleteDataOrmas(string kodeOrmas)
+        {
+            GeneralReturnValue res = new GeneralReturnValue();
+
+            try
+            {
+                var data = _dbContext.DT_Ormas.FirstOrDefault(n =>
+                n.kodeOrmas == kodeOrmas);
+
+                List<string> datax = new List<string>();
+
+                if (data != null)
+                {
+                    datax.Add(data.kodeOrmas);
+                    datax.Add(data.namaOrmas);
+
+                    _dbContext.DT_Ormas.Remove(data);
+                    _dbContext.SaveChanges();
+                }
+
+                res.status = "success";
+                res.message = "Data Ormas berhasil dihapus";
+                res.data = datax;
+            }
+            catch (Exception ex)
+            {
+                res.status = "failed";
+                res.message = ex.Message;
+                res.data = null;
+            }
+
+            return res;
+        }
+    }
+}
